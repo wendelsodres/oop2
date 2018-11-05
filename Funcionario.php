3a199cea2fce187116b881800a62097576a6536a
Wendel Sodre <wendelsodres@gmail.com>
2018-11-05 19:43:44 -0200

    Adicionando os atributos salario e departamento

--- Funcionario.php
+++ Funcionario.php
@@ -7,9 +7,28 @@ class Funcionario extends Pessoa{
     private $salario;
     private $departamento;
 
+    public function getSalario(){
+        return $this->salario;        
+    }
+
+    public function setSalario($val){
+        $this->salario = $val;
+    }
+
+    public function getDepartamento(){
+        return $this->departamento;        
+    }
+
+    public function setDepartamento($departa){
+        $this->departamento = $departa;
+    }
+
 }
 
 $funcionario = new Funcionario();
+
+$funcionario->setSalario(3500);
+$funcionario->setDepartamento('Desenvolvimento');
 
 $funcionario->setNome('Wendel Sodré');
 
@@ -18,4 +37,6 @@ $funcionario->setEndereco('Rua. Ipecaeta -  Vila Perus , São Paulo');
 echo "
     <p><strong>Nome: </strong>: {$funcionario->getNome()}</p>
     <p><strong>Endereço: </strong>: {$funcionario->getEndereco()}</p>
+    <p><strong>Salário: </strong>: {$funcionario->getSalario()}</p>
+    <p><strong>Departamento: </strong>: {$funcionario->getDepartamento()}</p>
 ";
