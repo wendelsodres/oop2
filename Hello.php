7396268092ed763e66ede13f1d0d63d733fe90f3
Wendel Sodre <wendelsodres@gmail.com>
2018-11-05 18:59:56 -0200

    Mensagem de boas vindas

---
+++ Hello.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WSS - Bem Vindo</title>
+</head>
+<body>
+
+<?php
+
+    echo "<h1>Seja Bem vindo !</h1>";
+?>
+
+</body>
+</html>
+
+
