bdc6f1867be960c9808baf6f63edf84ebd8dcc3d
Wendel Sodre <wendelsodres@gmail.com>
2018-11-10 15:00:35 -0200

    Lista de funcionarios

---
+++ nomes_funcionarios.php
@@ -0,0 +1,7 @@
+<?php
+
+$funcionarios = array("Wendel sodre","Mariana Fekiz","João da Silva","Gabriel Santos","Neymar Junior","Rasmus Lerdorf","Paulo Henrique","Francisco","Joao Paulo","Airton");
+
+foreach($funcionarios as $func){
+    echo $func."<br>";
+}
